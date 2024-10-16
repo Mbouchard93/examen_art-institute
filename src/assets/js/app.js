c92f3e8c7cfdacd5ac14a635d26f6f9a5fdef28a
Mbouchard93 <mariiane.bouchard12@gmail.com>
2024-10-16 18:54:18 -0400

    jsDoc

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,3 @@
-
 /**
  * @type {HTMLElement}
  */
@@ -8,21 +7,39 @@ const containerArt = document.querySelector(".container-art");
  */
 const btnSubmit = document.querySelector(".btn");
 /**
- * @type {HTMLElement}
+ * @type {HTMLInputElement}
  */
 const input = document.querySelector(".value");
 
-
-const getFetch = () => {
+const fetchArtworks = () => {
+  /**
+   * @returns {Promise}
+   */
   fetch(`https://api.artic.edu/api/v1/artworks/search?q=${input.value}`)
+    /**
+     * @param {Response}
+     * @return {Promise}
+     */
     .then((response) => response.json())
+    /**
+     * @param {Object}
+     */
     .then((data) => {
       containerArt.innerHTML = ``;
       data.data.forEach((art) => {
         fetch(art.api_link)
+          /**
+           * @param {Response}
+           * @return {Promise}
+           */
           .then((response) => response.json())
+          /**
+           * @param {Object}
+           */
           .then((data) => {
-
+            /**
+             * @type {HTMLElement}
+             */
             const artContainer = document.createElement("div");
             artContainer.className =
               "flex flex-col w-[350px] h-[450px] justify-around items-center p-5 bg-neutral-100 rounded-md  ";
@@ -50,7 +67,8 @@ const getFetch = () => {
             dialog.setAttribute("id", `dialog-${data.data.id}`);
 
             const overlay = document.createElement("div");
-            overlay.className = "overlay bg-white p-6 overflow-scroll w-1/2 gap-2";
+            overlay.className =
+              "overlay bg-white p-6 overflow-scroll w-1/2 gap-2";
 
             const closeButton = document.createElement("button");
             closeButton.textContent = "Fermer";
@@ -58,7 +76,7 @@ const getFetch = () => {
             overlay.appendChild(closeButton);
 
             const dialogTitle = document.createElement("p");
-            dialogTitle.className = 'text-xl';
+            dialogTitle.className = "text-xl";
             dialogTitle.textContent = data.data.title;
             overlay.appendChild(dialogTitle);
 
@@ -71,11 +89,16 @@ const getFetch = () => {
             overlay.appendChild(dialogDescription);
 
             const dialogNameArtist = document.createElement("p");
-            dialogNameArtist.className = 'underline';
+            dialogNameArtist.className = "underline";
             dialogNameArtist.textContent = data.data.artist_title;
             overlay.appendChild(dialogNameArtist);
-
+            /**
+             * @param {string}
+             */
             data.data.term_titles.forEach((term) => {
+              /**
+               * @type {HTMLElement}
+               */
               const containerTerm = document.createElement("ul");
               containerTerm.className = "bg-slate-200 p-4";
               const terms = document.createElement("li");
@@ -86,9 +109,11 @@ const getFetch = () => {
 
             const dialogDate = document.createElement("p");
             dialogDate.textContent = data.data.date_start;
-            dialogDate.className = 'font-bold';
+            dialogDate.className = "font-bold";
             overlay.appendChild(dialogDate);
-
+            /**
+             * @param {string}
+             */
             data.data.material_titles.forEach((material) => {
               const containermaterial = document.createElement("ul");
               const materials = document.createElement("li");
@@ -101,7 +126,6 @@ const getFetch = () => {
             containerArt.appendChild(dialog);
           });
       });
-
       dialogEvent();
     });
 };
@@ -130,7 +154,7 @@ const dialogEvent = () => {
 };
 
 btnSubmit.addEventListener("click", () => {
-  getFetch();
+  fetchArtworks();
 });
 
-getFetch();
+fetchArtworks();
